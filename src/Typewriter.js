e228b6558e1fa0b9e1f5579d3e01c67118c633ae
sofiahag <sofia.h4glund@gmail.com>
2025-12-03 18:00:56 +0100

    add variants

--- src/Typewriter.js
+++ src/Typewriter.js
@@ -1,25 +1,40 @@
 let i = 0;
 let row_one = "A CRO Analyst with a background in Life Science";
-let row_two = "Passionate about Experimentation!";
+let row_two = "Let's roll out some test variants!";
+let row_three = "👇";
 let speed = 100;
 let typingRow = 1;
 
 function typeWriter() {
+    const typingEl = document.getElementById("typing");
+
     if (typingRow === 1 && i < row_one.length) {
-        document.getElementById("typing").innerHTML += row_one.charAt(i);
+        typingEl.innerHTML += row_one.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     } 
     else if (typingRow === 1 && i === row_one.length) {
-        document.getElementById("typing").innerHTML += "<br/>";
+        typingEl.innerHTML += "<br/><span style='font-weight: bold;' id='row-two'></span>"; 
         typingRow = 2;
         i = 0;
         setTimeout(typeWriter, speed);
     } 
     else if (typingRow === 2 && i < row_two.length) {
-        document.getElementById("typing").innerHTML += row_two.charAt(i);
+        document.getElementById("row-two").innerHTML += row_two.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    else if (typingRow === 2 && i === row_two.length) {
+        typingEl.innerHTML += "<br/><span id='row-three'></span>";
+        typingRow = 3;
+        i = 0;
+        setTimeout(typeWriter, speed);
+    }
+    else if (typingRow === 3 && i < row_three.length) {
+        document.getElementById("row-three").innerHTML += row_three.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
 }
+
 typeWriter();
